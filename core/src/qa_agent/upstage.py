ba30373324e25a34626b1e29026554d6de44571a
pigpig1524 <vantuankiet.work@gmail.com>
2025-05-18 09:10:26 +0700

    update chat page, fix visualization

--- core/src/qa_agent/upstage.py
+++ core/src/qa_agent/upstage.py
@@ -40,15 +40,38 @@ Action: python_repl_ast
 Action Input: <your Python command here>
 """
 
+# CSV_PROMPT_SUFFIX = """
+# - **ALWAYS** before giving the Final Answer, try another method.
+# - **DO NOT MAKE UP AN ANSWER OR USE PRIOR KNOWLEDGE,
+# ONLY USE THE RESULTS OF THE CALCULATIONS YOU HAVE DONE**.
+# - **ALWAYS**, as part of your "Final Answer", explain how you got
+# to the answer on a section that starts with: "\\n\\nExplanation:\\n".
+# In the explanation, mention the column names that you used to get
+# to the final answer.
+# - Do not hallucinate or invent answers not in the data.
+# """
 CSV_PROMPT_SUFFIX = """
 - **ALWAYS** before giving the Final Answer, try another method.
+Then reflect on the answers of the two methods you did and ask yourself
+if it answers correctly the original question.
+If you are not sure, try another method.
+FORMAT 4 FIGURES OR MORE WITH COMMAS.
+- If the methods tried do not give the same result,reflect and
+try again until you have two methods that have the same result.
+- If you still cannot arrive to a consistent result, say that
+you are not sure of the answer.
+- If you are sure of the correct answer, create a beautiful
+and thorough response using Markdown.
 - **DO NOT MAKE UP AN ANSWER OR USE PRIOR KNOWLEDGE,
 ONLY USE THE RESULTS OF THE CALCULATIONS YOU HAVE DONE**.
 - **ALWAYS**, as part of your "Final Answer", explain how you got
-to the answer on a section that starts with: "\\n\\nExplanation:\\n".
+to the answer on a section that starts with: "\n\nExplanation:\n".
 In the explanation, mention the column names that you used to get
 to the final answer.
-- Do not hallucinate or invent answers not in the data.
+- If the user asks to visualize, plot, draw, chart, or show a graph:
+    - DO generate the appropriate Python code using matplotlib.
+    - DO include the code at the end of your "Final Answer" in Markdown response.
+    - Wrap the code in a proper Python Markdown code block
 """
 
 # # --- Streamlit App UI ---
@@ -133,5 +156,4 @@ class UpstageAgent:
             return response['output']
         except Exception as e:
             st.error(str(e))
-            st.info(SOLAR_API_KEY)
             return "Something went wrong when we process your request! Please try again!"
